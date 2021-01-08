74ce018dcd53697b45fc5fde3a7dc9e709f1f3b9
Ralph Moran <ralphmoran2003@gmail.com>
2021-01-08 12:37:56 -0800

    Added new setup when control name is not set

--- SplitTests/UATest1.php
+++ SplitTests/UATest1.php
@@ -13,7 +13,7 @@ class UATest1 extends AbstractTest
     public function run()
     {
         $this->sample(50)           # In this example, this AB test takes only 50% of the general traffic
-            ->control('UDF05')      # If the control variable does not exist, it's added to the payload.
+            // ->control('UDF05')      # If the control variable does not exist, it's added to the payload.
             ->groups([
                 'groupA'  => 20,    #
                 'groupB'  => 100,   # All groups are sorted by values.
